0e3f4a1009dbf16d868e0925fa91471c6bd8d603
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-14 17:40:38 +0300

    license information added

---
+++ b2_sdk/__main__.py
@@ -0,0 +1,9 @@
+######################################################################
+#
+# File: __main__.py
+#
+# Copyright 2018 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
