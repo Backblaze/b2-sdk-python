78a89005010f3c309bdb00c7c88221a44a4a47fc
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-14 17:36:37 +0300

    version set to 1.0.0

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.3.9',
+    version='1.0.0',
     description='Backblaze B2 SDK',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -135,7 +135,9 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={'b2-sdk': ['requirements.txt', 'requirements-test.txt', 'requirements-setup.txt']},
+    package_data={
+        'b2-sdk': ['requirements.txt', 'requirements-test.txt', 'requirements-setup.txt']
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
