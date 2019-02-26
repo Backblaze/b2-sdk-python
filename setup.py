4a4ad5fa148126e5ec448e8b71d8d1f7a6f4e2a6
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-26 09:19:25 +0300

    Directory b2_sdk renamed to b2sdk

--- setup.py
+++ setup.py
@@ -135,9 +135,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'b2sdk': ['requirements.txt', 'requirements-test.txt', 'requirements-setup.txt']
-    },
+    package_data={'b2sdk': ['requirements.txt', 'requirements-test.txt', 'requirements-setup.txt']},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
