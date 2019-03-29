2fe18ab25352a6210f4aa51d66ffeb85f6018de1
Michal Zukowski <michal.zukowski@reef.pl>
2019-03-29 16:51:30 +0100

    Version in setup.py from b2sdk.version

--- setup.py
+++ setup.py
@@ -51,13 +51,15 @@ with open('requirements-test.txt', encoding='utf-8') as f:
 with open('requirements-setup.txt', encoding='utf-8') as f:
     requirements_setup = f.read().splitlines()
 
+version = __import__('b2sdk.version').version.VERSION
+
 setup(
     name='b2sdk',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
+    version=version,
     description='Backblaze B2 SDK',
     long_description=long_description,
     long_description_content_type='text/markdown',
