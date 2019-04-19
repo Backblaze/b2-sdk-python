1a6f07b3093a4626536def28342b2503b12654ac
Pawel Polewicz <p.polewicz@gmail.com>
2019-04-19 21:44:28 +0200

    Make Codacy linters happy

--- test/test_version_utils.py
+++ test/test_version_utils.py
@@ -51,6 +51,7 @@ class TestRenameArgument(TestBase):
             @rename_argument('aaa', 'bbb', '0.1.0', '0.1.2')
             def late(bbb):
                 return bbb
+            assert late  # make linters happy
 
     def test_future_replacement(self):
         with self.assertRaises(
@@ -62,6 +63,7 @@ class TestRenameArgument(TestBase):
             @rename_argument('aaa', 'bbb', '0.2.0', '0.2.2')
             def early(bbb):
                 return bbb
+            assert early  # make linters happy
 
     def test_inverted_versions(self):
         with self.assertRaises(
@@ -73,6 +75,7 @@ class TestRenameArgument(TestBase):
             @rename_argument('aaa', 'bbb', '0.2.2', '0.2.0')
             def backwards(bbb):
                 return bbb
+            assert backwards  # make linters happy
 
 
 class TestRenameFunction(TestBase):
