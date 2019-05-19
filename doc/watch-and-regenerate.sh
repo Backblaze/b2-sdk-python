2b9f2d55736a1c833994a6ef00fafc428ffb51b1
Pawel Polewicz <p.polewicz@gmail.com>
2019-05-20 00:54:52 +0200

    Fix doc/watch-and-regenerate.sh

--- doc/watch-and-regenerate.sh
+++ doc/watch-and-regenerate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -eu
-sudo fatrace | awk '/.* CW \/home\/rare\/b2\/b2-sdk-python\/.*\.(rst|py)$/ {print;system("./regenerate.sh")}'
+sudo fatrace | awk '/.* CW .*\/b2-sdk-python\/.*\.(rst|py)$/ {print;system("./regenerate.sh")}'
