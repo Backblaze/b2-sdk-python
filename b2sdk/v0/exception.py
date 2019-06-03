66e2acc87af221165269e273e05284b77694a3a3
Pawel Polewicz <p.polewicz@gmail.com>
2019-06-03 03:33:49 +0200

    Add the interface

---
+++ b2sdk/v0/exception.py
@@ -0,0 +1,11 @@
+######################################################################
+#
+# File: b2sdk/v0/exceptin/__init__.py
+#
+# Copyright 2019 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
+from b2sdk.v1.exception import *
