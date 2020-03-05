10b9025bf9c6cd2069091062e2d9e37ffcf666c3
Michal Zukowski <michal.zukowski@reef.pl>
2020-03-05 12:30:31 +0100

    Fix stream ReadOnlyMixin write UnsupportedOperation message

--- b2sdk/stream/base.py
+++ b2sdk/stream/base.py
@@ -6,4 +6,4 @@ class ReadOnlyMixin(object):
         return False
 
     def write(self, data):
-        raise io.UnsupportedOperation('Stream with hash cannot be written to')
+        raise io.UnsupportedOperation('Cannot accept a write to a read-only stream')
