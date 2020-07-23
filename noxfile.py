244f1b10dcfe86d59e14fb213278e97ad920b2e6
Maciej Lech <maciej.lech@reef.pl>
2020-07-23 11:58:44 +0200

    Improve Nox test session

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,13 @@
+######################################################################
+#
+# File: noxfile.py
+#
+# Copyright 2020 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
 import os
 import subprocess
 
@@ -9,9 +19,12 @@ PY_PATHS = ['b2sdk', 'test', 'noxfile.py', 'setup.py']
 
 # TODO: remove nose and pyflakes
 REQUIREMENTS_FORMAT = ['docformatter==1.3.1', 'isort==5.1.1', 'yapf==0.27']
-REQUIREMENTS_LINT = [*REQUIREMENTS_FORMAT, 'pyflakes', 'flake8==3.8.3', 'liccheck==0.4.7']
+REQUIREMENTS_LINT = [*REQUIREMENTS_FORMAT, 'pyflakes', 'flake8==3.8.3']
 REQUIREMENTS_TEST = ['nose==1.3.7', 'pytest==5.4.3', 'pytest-cov==2.10.0']
-REQUIREMENTS_DOC = ['sphinx', 'sphinx-autobuild', 'sphinx_rtd_theme', 'sphinxcontrib-plantuml', 'sadisplay']
+REQUIREMENTS_BUILD = ['liccheck==0.4.7', 'setuptools>=20.2']
+REQUIREMENTS_DOC = [
+    'sphinx', 'sphinx-autobuild', 'sphinx_rtd_theme', 'sphinxcontrib-plantuml', 'sadisplay'
+]
 
 nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = [
@@ -19,7 +32,7 @@ nox.options.sessions = [
     'test',
 ]
 
-# use Python interpreter provided by GitHub Actions
+# In CI, use Python interpreter provided by GitHub Actions
 if CI:
     nox.options.force_venv_backend = 'none'
 
@@ -31,7 +44,7 @@ if CI:
 # noinspection PyShadowingBuiltins
 @nox.session(python=PYTHON_VERSIONS[-1])
 def format(session):
-    """Run code formatters."""
+    """Format the code."""
     session.install(*REQUIREMENTS_FORMAT)
     # TODO: incremental mode for yapf
     session.run('yapf', '--in-place', '--parallel', '--recursive', *PY_PATHS)
@@ -73,8 +86,6 @@ def lint(session):
         print('\n'.join(output))
         session.error('pyflakes has failed')
     # session.run('flake8', *PY_PATHS)
-    session.run('liccheck', '-s', 'setup.cfg', silent=True)
-    session.run('python', 'setup.py', 'check', '--metadata', '--strict', silent=True)
 
 
 #
@@ -84,18 +95,24 @@ def lint(session):
 
 @nox.session(python=PYTHON_VERSIONS)
 def test(session):
-    """Run unit tests."""
+    """Run test suites."""
     session.install('-e', '.', *REQUIREMENTS_TEST)
-    tests = session.posargs or ['test']
-    session.run('pytest', '--cov=b2sdk', '--cov-report=', '--doctest-modules', *tests)
-    session.notify('cover')
+
+    if session.posargs:
+        # Run given test suite
+        session.run('pytest', '--cov=b2sdk', '--cov-report=', *session.posargs)
+    else:
+        # By default, run all suites and show the coverage
+        session.run('pytest', '--cov=b2sdk', '--cov-report=', '--doctest-modules', 'test/unit')
+        session.run('pytest', 'test/integration')
+        session.notify('cover')
 
 
 @nox.session
 def cover(session):
     """Perform coverage analysis."""
     session.install('coverage')
-    session.run('coverage', 'report', '--fail-under=77', '--show-missing')
+    session.run('coverage', 'report', '--fail-under=75', '--show-missing')
     session.run('coverage', 'erase')
 
 
@@ -106,9 +123,11 @@ def cover(session):
 
 @nox.session(python=PYTHON_VERSIONS[-1])
 def build(session):
-    """Build a distribution."""
+    """Build the distribution."""
     # TODO: consider using wheel as well
-    session.install('setuptools')
+    session.install('-e', '.', *REQUIREMENTS_BUILD)
+    session.run('liccheck', '-s', 'setup.cfg')
+    session.run('python', 'setup.py', 'check', '--metadata', '--strict')
     session.run('rm', '-rf', 'build', 'dist', 'b2sdk.egg-info', external=True)
     session.run('python', 'setup.py', 'sdist', *session.posargs)
 
@@ -124,10 +143,8 @@ def deploy(session):
 def doc(session):
     """Build the documentation."""
     session.install('-e', '.', *REQUIREMENTS_DOC)
-
     session.cd('doc')
     sphinx_args = ['-b', 'html', '-T', '-W', 'source', 'build/html']
-
     session.run('rm', '-rf', 'build', external=True)
     if not session.interactive:
         sphinx_cmd = 'sphinx-build'
