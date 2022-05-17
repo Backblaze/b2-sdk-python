0b389bc49da4793a44b363996b407059e83daf17
Aleksandr Goncharov <aleksandr.goncharov@reef.pl>
2022-05-17 17:23:32 +0300

    Move chunks to `scan` module

--- b2sdk/scan/__init__.py
+++ b2sdk/scan/__init__.py
@@ -1,15 +1,9 @@
 ######################################################################
 #
-# File: b2sdk/sync/exception.py
+# File: b2sdk/scan/__init__.py
 #
-# Copyright 2019 Backblaze Inc. All Rights Reserved.
+# Copyright 2022 Backblaze Inc. All Rights Reserved.
 #
 # License https://www.backblaze.com/using_b2_code.html
 #
 ######################################################################
-
-from ..exception import B2SimpleError
-
-
-class IncompleteSync(B2SimpleError):
-    pass
