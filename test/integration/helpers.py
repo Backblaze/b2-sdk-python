cb481978b3e136fe9877785d56663d94c5a11f41
Pawel Polewicz <p.polewicz@gmail.com>
2022-06-23 19:05:52 +0200

    Fix download integration tests on non-production environments

--- test/integration/helpers.py
+++ test/integration/helpers.py
@@ -9,6 +9,7 @@
 ######################################################################
 
 from typing import Optional
+import os
 import random
 import string
 
@@ -29,5 +30,6 @@ def bucket_name_part(length):
 def authorize(b2_auth_data, api_config=DEFAULT_HTTP_API_CONFIG):
     info = InMemoryAccountInfo()
     b2_api = B2Api(info, api_config=api_config)
-    b2_api.authorize_account("production", *b2_auth_data)
+    realm = os.environ.get('B2_TEST_ENVIRONMENT', 'production')
+    b2_api.authorize_account(realm, *b2_auth_data)
     return b2_api, info
