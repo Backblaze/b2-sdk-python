2fd91c447a9ec297851dc94f839fa656abea55fe
Krzysztof Kalinowski <krzysztof.kalinowski@reef.pl>
2023-01-11 13:43:06 +0100

    Copyright bumped to trigger CI workflow

--- b2sdk/utils/__init__.py
+++ b2sdk/utils/__init__.py
@@ -2,7 +2,7 @@
 #
 # File: b2sdk/utils/__init__.py
 #
-# Copyright 2019 Backblaze Inc. All Rights Reserved.
+# Copyright 2022 Backblaze Inc. All Rights Reserved.
 #
 # License https://www.backblaze.com/using_b2_code.html
 #
