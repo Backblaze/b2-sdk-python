6a1fbcafbd22fbf78b3ed6d8a3762637d574e920
Maciej Urbanski <maciej.rooter.urbanski@reef.pl>
2024-07-24 15:25:41 +0200

    add pytest-apiver plugin compatiblity fixture

--- test/conftest.py
+++ test/conftest.py
@@ -16,3 +16,15 @@ import pytest
 def bg_executor():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         yield executor
+
+
+@pytest.fixture
+def apiver_module():
+    """
+    b2sdk apiver module fixture.
+
+    A compatibility function that is to be replaced by `pytest-apiver` plugin in the future.
+    """
+    import apiver_deps  # noqa: F401
+
+    return apiver_deps
