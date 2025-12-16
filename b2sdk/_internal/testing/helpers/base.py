191fec31cc0598a7e9051d400c9482381b364c9c
Olzhas Arystanov <olzhas.arystanov@reef.pl>
2025-12-16 19:59:11 +0500

    Improve debugging in integration tests BucketManager

--- b2sdk/_internal/testing/helpers/base.py
+++ b2sdk/_internal/testing/helpers/base.py
@@ -13,7 +13,6 @@ import pytest
 
 from b2sdk._internal.testing.helpers.bucket_manager import BucketManager
 from b2sdk.v2 import B2Api
-from b2sdk.v2.exception import DuplicateBucketName
 
 
 @pytest.mark.usefixtures('cls_setup')
@@ -47,30 +46,6 @@ class IntegrationTestBase:
             written += line_len
 
     def create_bucket(self):
-        bucket_name = self.bucket_manager.new_bucket_name()
-        try:
-            bucket = self.bucket_manager.create_bucket(name=bucket_name)
-        except DuplicateBucketName:
-            self._duplicated_bucket_name_debug_info(bucket_name)
-            raise
+        bucket = self.bucket_manager.create_bucket()
         self.buckets_created.append(bucket)
         return bucket
-
-    def _duplicated_bucket_name_debug_info(self, bucket_name: str) -> None:
-        # Trying to obtain as much information as possible about this bucket.
-        print(' DUPLICATED BUCKET DEBUG START '.center(60, '='))
-        bucket = self.b2_api.get_bucket_by_name(bucket_name)
-
-        print('Bucket metadata:')
-        bucket_dict = bucket.as_dict()
-        for info_key, info in bucket_dict.items():
-            print(f'\t{info_key}: "{info}"')
-
-        print('All files (and their versions) inside the bucket:')
-        ls_generator = bucket.ls(recursive=True, latest_only=False)
-        for file_version, _directory in ls_generator:
-            # as_dict() is bound to have more info than we can use,
-            # but maybe some of it will cast some light on the issue.
-            print(f'\t{file_version.file_name} ({file_version.as_dict()})')
-
-        print(' DUPLICATED BUCKET DEBUG END '.center(60, '='))
